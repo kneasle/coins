1ef42a6b1acca9b514c2157bc2ce1a58e73fd6af
Kneasle <kneasle@gmail.com>
2023-11-25 22:40:36 +0000

    Write smallest denoms to a file

--- read.py
+++ read.py
@@ -8,7 +8,7 @@ import sys
 
 def main():
     wiki_html = open("source-table.html").read()
-    currencies = parse_main_wiki_page(wiki_html)
+    currencies = parse_main_wiki_page(wiki_html) # [145:]
 
     # If args are supplied, only use currencies which contain text of at least one of these args
     if len(sys.argv) > 1:
@@ -19,12 +19,25 @@ def main():
             return False
         currencies = [c for c in currencies if in_args(c)]
 
+    smallest_denoms = []
     for count, c in enumerate(currencies):
         slug = c["page_slug"]
+        name = c["name"]
         if slug == "Bitcoin":
+            # Bitcoin is apparently legal tender in some countries, but has no coins and so can't
+            # be collected
             continue
         count_string = f"{count}/{len(currencies)}"
-        smallest_denom = get_smallest_denomination(slug, c["name"], count_string)
+
+        sorted_denoms = get_sorted_denominations(slug, name, count_string)
+        smallest_denoms.append((name, sorted_denoms))
+        contents = file_contents(smallest_denoms)
+        with open("out.txt", "w") as f:
+            f.write(contents)
+
+#####################
+# PARSING MAIN PAGE #
+#####################
 
 def parse_main_wiki_page(wiki_html):
     # This code parses the table from this wiki page:
@@ -75,17 +88,16 @@ def parse_main_wiki_page(wiki_html):
         currencies.append(obj)
     return currencies
 
-def get_smallest_denomination(page_slug, name, count_string):
+################################
+# PARSING EACH CURRENCY'S PAGE #
+################################
+
+def get_sorted_denominations(page_slug, name, count_string):
     print()
     print()
     print()
     print("------------------------------------")
 
-    if page_slug in ["Russian_ruble"]:
-        return
-
-    # print()
-    # print()
     # Check for cached page
     cache_path = f".wiki_cache/{page_slug}.html"
     url = "http://en.wikipedia.org/wiki/" + page_slug
@@ -108,25 +120,28 @@ def get_smallest_denomination(page_slug, name, count_string):
     singular_name = name.split(" ")[-1]
     units[singular_name] = 1
     # Special cases for 1 and 0.01 denominations which don't appear in the wiki pages
-    for one_unit in ["¥ rmb", "$", "pesos dominicano", "rls", "ll"]:
+    for one_unit in ["¥ rmb", "$", "pesos dominicano", "rls", "ll", "ls"]:
         units[one_unit] = 1
-    for cent_unit in ["¢", "centavos", "cop", "st", "pt"]:
+    for cent_unit in ["¢", "centavos", "cop", "st", "pt", "kopeck"]:
         units[cent_unit] = 0.01
 
     # Parse the infobox
     infobox = soup.find("table", class_="infobox")
 
     # Parse 'Code' out of `ISO 4271`
-    iso_elem = infobox.find("th", string="ISO 4217")
-    parsed_iso = parse_infobox(iso_elem, "Denominations")
-    iso_code = parsed_iso["Code"].text.split("(")[0].strip()
+    iso_elem = infobox.find("th", string="ISO 4217").parent
+    parsed_iso = parse_infobox(iso_elem, ["Denominations", "Unit"])
+    if "Code" in parsed_iso:
+        iso_code_elem, _others = parsed_iso["Code"]
+        iso_code = iso_code_elem.text.split("(")[0].strip()
+        units[iso_code] = 1
 
     # Parse the `Unit` section for symbols and plurals
     unit_elem = infobox.find("th", string="Unit")
     if unit_elem is None:
         print("BRUH!  {url} has no normal unit symbol")
     else:
-        parsed_unit = parse_infobox(unit_elem.parent, "Denominations")
+        parsed_unit = parse_infobox(unit_elem.parent, ["Denominations"])
         for key in ["Symbol", "Plural"]:
             if key not in parsed_unit:
                 continue
@@ -143,7 +158,7 @@ def get_smallest_denomination(page_slug, name, count_string):
     # Parse the `denominations` section for subunits
     parsed_denoms = parse_infobox(
         soup.find("a", string="Denominations").parent.parent,
-        "Demographics"
+        ["Demographics"]
     )
     if "Subunit" in parsed_denoms:
         _, subunits = parsed_denoms["Subunit"]
@@ -152,7 +167,7 @@ def get_smallest_denomination(page_slug, name, count_string):
             # Get subunit amount
             fraction_num, fraction_den = fraction_elem.text.split("⁄")
             amount = int(fraction_num) / int(fraction_den.replace(",", ""))
-            for name in parse_subunit(name_elem):
+            for name in read_unit_names(name_elem):
                 subunit_names.append(name)
                 units[name] = amount
         # Add plurals and symbols (if they exist)
@@ -161,8 +176,8 @@ def get_smallest_denomination(page_slug, name, count_string):
                 continue
             _, sections = parsed_denoms[key]
             for subunit_elem, other_name_elem in sections:
-                subunit = parse_subunit(subunit_elem)[0]
-                for other_name in parse_subunit(other_name_elem):
+                subunit = read_unit_names(subunit_elem)[0]
+                for other_name in read_unit_names(other_name_elem):
                     if other_name != "" and subunit in units:
                         units[other_name] = units[subunit]
     # Add normal plurals by adding 's' to everything (even if it's already a plural)
@@ -193,10 +208,13 @@ def get_smallest_denomination(page_slug, name, count_string):
 
     print(denoms)
     print("Smallest:", denoms[0])
+    return denoms
 
 
-def parse_subunit(elem):
+def read_unit_names(elem):
     subunit = elem.text
+    # subunit = subunit.replace("\u200e", "")
+    # subunit = subunit.replace("numeric: ", "")
     subunit = re.sub("\[.*?\]", "", subunit) # Remove references
     subunit = subunit.split('"')[0] # strip after "
     subunit = subunit.strip()
@@ -219,7 +237,6 @@ def parse_denoms(elem, type, is_rare, units):
     text = re.sub(r"\(.*\)", "", text) # Remove languages
     if text == "":
         return []
-    print()
     print(text.__repr__())
 
     named_denoms = re.split(", | & |;|\\band\\b", text)
@@ -305,7 +322,7 @@ def parse_fraction(text):
     return total
 
 # Returns a type {string: (elem, [(elem, elem)])}
-def parse_infobox(heading_elem, next_section_name):
+def parse_infobox(heading_elem, next_section_names):
     headings = {}
 
     current_heading = None
@@ -317,7 +334,8 @@ def parse_infobox(heading_elem, next_section_name):
         headings[current_heading] = (current_right_elem, current_rows)
 
     row = heading_elem.next_sibling
-    while next_section_name not in row.text:
+    while row.text.strip() not in next_section_names:
+        # Check if this row's text contains one of `next_section_names`
         left, right = row.children
         heading = left.text.strip()
         is_bold = left.find("span", class_="nobold") is None
@@ -338,6 +356,40 @@ def parse_infobox(heading_elem, next_section_name):
     add_previous_headings()
 
     return headings
+
+################
+# SAVE TO FILE #
+################
+
+def file_contents(all_denoms):
+    longest_name_len = max([len(name) for name, _ in all_denoms])
+
+    s = ""
+    for name, denoms in all_denoms:
+        # Separate a run of rare denominations, which would technically have the lowest values but
+        # are so hard to collect that I consider them to be optional
+        rare_denoms = []
+        first_common_denom = None
+        for d in denoms:
+            is_rare = d[3]
+            if not is_rare:
+                first_common_denom = d
+                break
+            rare_denoms.append(d)
+        # Build the string
+        s += f"{name} {'.' * (longest_name_len - len(name))}... "
+        if rare_denoms != []:
+            rare_denoms_string = ", ".join([denom_name(d) for d in rare_denoms])
+            s += f"({rare_denoms_string}) "
+        if first_common_denom is not None:
+            s += denom_name(first_common_denom)
+        s += "\n"
+    return s
+
+def denom_name(denom):
+    _value, name, denom_type, _is_rare = denom
+    return f"{name} [note]" if denom_type != "Coin" else name;
+
 
 if __name__ == "__main__":
     main()
